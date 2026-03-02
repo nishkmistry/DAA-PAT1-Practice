42df7a91850637b80d121ab3d8d7551b411b5d3b
Mistry Nishk Nileshkumar <nishkmistry@gmail.com>
2026-03-02 23:43:30 +0530

    Removed the deadcode from all the files.

--- ImportantFolder/KDP.cpp
+++ ImportantFolder/KDP.cpp
@@ -31,21 +31,8 @@ int main()
             items.push_back({w, v, (double)v / (double)w});
         }
     }
-    sort(items.begin(), items.end(), [](const Item &a, const Item &b)
-         {
-if(a.r!=b.r) return a.r>b.r;
-if(a.v!=b.v) return a.v>b.v;
-return a.w<b.w; });
     double rem = (double)W;
     double fracTotal = (double)zeroValueSum;
-    for (auto &it : items)
-    {
-        if (rem <= 0)
-            break;
-        double take = min(rem, (double)it.w);
-        fracTotal += take * it.r;
-        rem -= take;
-    }
     vector<long long> dp(W + 1, 0);
     for (auto &it : items)
     {
